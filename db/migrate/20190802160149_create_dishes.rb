1659831cdae36e8786191f352d5c645b9a7a1d0b
Jelani Woods <jelani@firstdraft.com>
2019-08-02 16:27:07 +0000

    wip

---
+++ db/migrate/20190802160149_create_dishes.rb
@@ -0,0 +1,11 @@
+class CreateDishes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dishes do |t|
+      t.string :name
+      t.string :description
+      t.boolean :review
+
+      t.timestamps
+    end
+  end
+end
